6c4a82bb5f8df49c2b37ccc162adb1cb9daf4099
Serjes <serjes@yandex.ru>
2018-09-17 18:23:44 +0400

    issue of the adding books was solved, but there is issue of deleting now

--- dz14/src/main/java/ru/otus/dz14/controller/PagesController.java
+++ dz14/src/main/java/ru/otus/dz14/controller/PagesController.java
@@ -5,19 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.dz14.domain.Book;
+import ru.otus.dz14.domain.BookDto;
 import ru.otus.dz14.repository.BookRepository;
+import ru.otus.dz14.service.LibraryService;
 
 import java.util.List;
 
 @Controller
 public class PagesController {
 
-//    @Autowired
+    //    @Autowired
     private final BookRepository bookRepository;
+    private final LibraryService libraryService;
 
     @Autowired
-    public PagesController(BookRepository bookRepository) {
+    public PagesController(BookRepository bookRepository, LibraryService libraryService) {
         this.bookRepository = bookRepository;
+        this.libraryService = libraryService;
     }
 
 
@@ -27,14 +31,25 @@ public class PagesController {
     }
 
     @GetMapping("/books")
-    public String booksPage(Model model) {
+    public String booksPage(Model model, int id) {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
+        model.addAttribute("id", id);
         return "books";
     }
 
+    @GetMapping("/books/delete/{id}")
+    public String delete(@PathVariable("id") int id)
+    {
+        libraryService.delBook(id);
+        return "redirect:/books";
+    }
+
+
     @GetMapping("/addbook")
     public String addBookPage(Model model) {
+        BookDto bookDto = new BookDto();
+        model.addAttribute("bookDto", bookDto);
 //        List<Book> books = bookRepository.findAll();
 //        model.addAttribute("books", books);
         return "addbook";
@@ -53,10 +68,25 @@ public class PagesController {
 //        return PersonDto.toDto(accountWithId);
 //    }
 
-//    @GetMapping("/authenticated")
+    //    @GetMapping("/authenticated")
 //    public String authenticatedPage() {
 //        return "authenticated";
 //    }
-
-//    return "redirect:/book"
+    @RequestMapping(
+            value = {"/books/add"},
+            method = RequestMethod.POST
+    )
+    public String saveBook(
+            Model model,
+            @ModelAttribute("bookDto") BookDto bookDto
+//            @RequestBody("bookDto") BookDto bookDto
+//            @RequestAttribute("bookDto") BookDto bookDto
+    ) {
+//        Book book = libraryService.
+        libraryService.addBook(bookDto.getBookTitle(),
+                bookDto.getAuthorName(),
+                bookDto.getAuthorLastName(),
+                bookDto.getGenre());
+        return "redirect:/books";
+    }
 }
--- dz14/src/main/java/ru/otus/dz14/domain/BookDto.java
+++ dz14/src/main/java/ru/otus/dz14/domain/BookDto.java
@@ -1,4 +1,45 @@
 package ru.otus.dz14.domain;
 
+@SuppressWarnings("all")
 public class BookDto {
+
+    private String bookTitle;
+    private String authorName;
+    private String authorLastName;
+    private String genre;
+
+    public BookDto() {
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
