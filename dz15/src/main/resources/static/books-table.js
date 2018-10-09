637c8258d3e8c4da2972cce1b5d5ac726c701441
Serjes <serjes@yandex.ru>
2018-10-09 18:33:20 +0400

    trouble with deleting book

---
+++ dz15/src/main/resources/static/books-table.js
@@ -0,0 +1,71 @@
+// $(document).ready(function () {
+//
+//     // $('#bookTable').load("/books-table");
+//     $("#adding-form").submit(function (event) {
+//         event.preventDefault();
+//         adding_book_submit();
+//     });
+// });
+
+function adding_book_submit(){
+
+    var addingFormData = {
+        bookTitle :  $("#bookTitle-input").val(),
+        authorName: $("#authorName").val(),
+        authorLastName :  $("#authorLastName").val(),
+        genre : $("#genre").val(),
+        author : $("#author").val()
+    }
+
+    $("#adding-form").prop("disabled", true);
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/books",
+        data: JSON.stringify(addingFormData),
+        dataType: 'json',
+        cache: false,
+        // timeout: 600000,
+        success: function (data) {
+
+            // var json = "<h4>Ajax Response</h4><pre>"
+            //     + JSON.stringify(data, null, 4) + "</pre>";
+            // $('#feedback').html(json);
+            // window.location.href = "/books";
+            // document.getElementById("books").onload;
+            // var jsonBooks = JSON.stringify(data);
+            // $('#table-books').html(json);
+            // $('#bookTable').load("/books-table #table-books");
+            $('#bookTable').load("/books-table");
+
+            console.log("SUCCESS : ", data);
+            // $("#btn-search").prop("disabled", false);
+            $("#adding-form").prop("disabled", false);
+
+        },
+        error: function (e) {
+
+            // var json = "<h4>Ajax Response</h4><pre>"
+            //     + e.responseText + "</pre>";
+            // $('#feedback').html(json);
+
+            console.log("ERROR : ", e);
+            // $("#btn-search").prop("disabled", false);
+            $("#adding-form").prop("disabled", false);
+
+        }
+    });
+}
+
+function deleteBook(id) {
+    // alert("delete book!");
+    // var bookidval = document.getElementById("book");
+    // var object = bookidval.valueOf();
+    // var bookData = {
+    //     bookId :  $("#book").val()
+    // }
+
+    // alert(object);
+    alert(id);
+}
